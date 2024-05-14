b3e93fb094e19db9cecb21b0ffd51a26860f9133
xTrzca <contactadam2006@gmail.com>
2024-05-14 10:11:16 -0400

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='doom clone',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='trzca',
+    author_email='',
+    description=''
+)
