41d2e8c5e0f64887000d687060699b31588687c5
xTrzca <contactadam2006@gmail.com>
2024-05-16 10:47:12 -0400

    reformat

--- object_renderer.py
+++ object_renderer.py
@@ -15,12 +15,15 @@ class ObjectRenderer:
                              for i in range(11)]
         self.digits = dict(zip(map(str, range(11)), self.digit_images))
         self.game_over_image = self.get_texture('resources/textures/game_over.png', RES)
-
+        self.win_image = self.get_texture('resources/textures/win.png', RES)
 
     def draw(self):
         self.draw_background()
         self.render_game_objects()
         self.draw_player_health()
+
+    def win(self):
+        self.screen.blit(self.win_image, (0, 0))
 
     def game_over(self):
         self.screen.blit(self.game_over_image, (0, 0))
@@ -35,17 +38,16 @@ class ObjectRenderer:
         self.screen.blit(self.blood_screen, (0, 0))
 
     def draw_background(self):
-        self.sky_offset = (self.sky_offset + 4.0 * self.game.player.rel) % WIDTH
+        self.sky_offset = (self.sky_offset + 4.5 * self.game.player.rel) % WIDTH
         self.screen.blit(self.sky_image, (-self.sky_offset, 0))
         self.screen.blit(self.sky_image, (-self.sky_offset + WIDTH, 0))
-        #floor
-        pg.draw.rect(self.screen,  FLOOR_COLOUR, (0, HALF_HEIGHT, WIDTH, HALF_HEIGHT))
-
+        # floor
+        pg.draw.rect(self.screen, FLOOR_COLOUR, (0, HALF_HEIGHT, WIDTH, HEIGHT))
 
     def render_game_objects(self):
-            list_objects = sorted(self.game.raycasting.objects_to_render, key=lambda t: t[0], reverse=True)
-            for depth, image, pos in list_objects:
-                self.screen.blit(image, pos)
+        list_objects = sorted(self.game.raycasting.objects_to_render, key=lambda t: t[0], reverse=True)
+        for depth, image, pos in list_objects:
+            self.screen.blit(image, pos)
 
     @staticmethod
     def get_texture(path, res=(TEXTURE_SIZE, TEXTURE_SIZE)):
@@ -60,4 +62,3 @@ class ObjectRenderer:
             4: self.get_texture('resources/textures/4.png'),
             5: self.get_texture('resources/textures/5.png'),
         }
-
